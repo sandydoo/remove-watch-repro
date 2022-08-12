53d2f1cdf6ee98752e898202cddc55e317dd6a92
sandydoo <hey@sandydoo.me>
2022-08-12 15:12:39 +0000

    Use signals to cause produce a bad descriptor error

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,26 @@
 module Main where
 
 import Control.Concurrent
-import Control.Exception (bracket)
-import qualified Control.Exception
-import Control.Monad (replicateM, void)
+import qualified Control.Concurrent.Async as Async
+import Control.Exception.Safe
+import Control.Monad (forever, replicateM, void)
 import qualified System.Directory as FS
 import qualified System.FSNotify as FS
 import System.FilePath (FilePath, (</>))
 import qualified System.IO.Temp as Temp
+import qualified System.Posix.Signals as Signals
 import qualified System.Random as Random
 
 main :: IO ()
 main =
   withRandomTempDirectory $ \watchedDir -> do
-    let testDir = watchedDir </> "test"
-    FS.createDirectory testDir
-    writeFile (testDir </> "testfile") "boop"
+    FS.withManager $ \mgr -> do
+      stop <- FS.watchDir mgr watchedDir (const True) print
+      let signalset = Signals.CatchOnce stop
+      void $ Signals.installHandler Signals.sigINT signalset Nothing
+      void $ Signals.installHandler Signals.sigTERM signalset Nothing
 
-    void . forkIO $
-      FS.withManager $ \mgr ->
-        bracket
-          (FS.watchDir mgr watchedDir (const True) print)
-          void
-          id
-
-    FS.removeDirectoryRecursive testDir
+    Async.concurrently (Signals.raiseSignal Signals.sigINT) (Signals.raiseSignal Signals.sigTERM)
 
     threadDelay (2 * 1000 * 1000)
 
